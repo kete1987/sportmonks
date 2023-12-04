7b6332f1d1a22f49874daf6724dd76aa3363b896
Ivan Morales <kete1987@gmail.com>
2023-12-04 23:13:56 +0100

    update

--- src/main/java/es/com/kete1987/sportmonks/library/v3/util/EventType.java
+++ src/main/java/es/com/kete1987/sportmonks/library/v3/util/EventType.java
@@ -2,6 +2,7 @@ package es.com.kete1987.sportmonks.library.v3.util;
 
 public class EventType {
     public static int VAR = 10;
+    public static int VAR_CARD = 1697;
     // At the moment we have the var events Red card cancelled Penalty Disallowed Penalty confirmed Card adjusted Card reviewed Card upgrade Goal cancelled Goal confirmed and Goal under review.
     public static int GOAL = 14;
     public static int OWNGOAL = 15;
